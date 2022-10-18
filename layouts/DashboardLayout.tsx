f1848bbfdd0d6df67c400a315faacb18ea5119ad
amani <amani@happymonk.co>
2022-10-18 10:26:12 +0200

    ft(layout): add app layout

---
+++ layouts/DashboardLayout.tsx
@@ -0,0 +1,29 @@
+// import Map from "../components/map/Map";
+import Map from "../components/map/Map";
+import styles from "./index.module.scss";
+
+const DashboardLayout = () => {
+  return (
+    <div className={styles.dashboard_layout}>
+      <div className={styles.map_content}>
+        <Map />
+      </div>
+      <div className={styles.data_content}>
+        <div className={styles.left_side}>
+          <div className={styles.content}>
+            <div className={styles.left}>
+              <div className={styles.search_bar}></div>
+              <div className={styles.videos}></div>
+            </div>
+            <div className={styles.right}>
+              <div className={styles.map_info}></div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.right_side}></div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
