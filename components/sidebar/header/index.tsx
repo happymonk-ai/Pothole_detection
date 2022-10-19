fad9de45ee816eb7973b609fc0cac606e07686ac
amani <amani@happymonk.co>
2022-10-20 00:38:19 +0200

    ft(detections): add detection details views

--- components/sidebar/header/index.tsx
+++ components/sidebar/header/index.tsx
@@ -2,29 +2,45 @@ import Image from "next/image";
 import { images } from "../../../constants/images";
 import styles from "./index.module.scss";
 import common_styles from "../../../styles/common.module.scss";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setSidebarShareIsOpen } from "../../../redux/modules/actions/sidebarShareSlice";
+import { RootState } from "../../../redux/store";
+import { setDetailView } from "../../../redux/modules/actions/detectionsSlice";
 
 const SidebarHeader = () => {
+  const { isDetailView } = useSelector((state: RootState) => state.detections);
+
   const dispatch = useDispatch();
 
   const handleOpen = () => {
     dispatch(setSidebarShareIsOpen(true));
   };
 
+
+  const handleDetailsClose = () => {
+    dispatch(setDetailView(false))
+  }
+
   return (
     <div className={styles.container}>
-      <div className={`${styles.left} ${common_styles.sidebar_main_header}`}>
-        Potholes Metrics
+      <div className={`${styles.left}`}>
+        {isDetailView && (
+          <Image src={images.arrow_left} alt="" className={styles.icon}  onClick={handleDetailsClose}/>
+        )}
+        <h1 className={`${styles.title}  ${common_styles.sidebar_main_header}`}>
+          {(isDetailView && "Pothole detected") || "Potholes Metrics"}
+        </h1>
       </div>
-      <div className={styles.right}>
-        <Image
-          src={images.share_green}
-          alt=""
-          onClick={handleOpen}
-          className={styles.icon}
-        />
-      </div>
+      {!isDetailView && (
+        <div className={styles.right}>
+          <Image
+            src={images.share_green}
+            alt=""
+            onClick={handleOpen}
+            className={styles.icon}
+          />
+        </div>
+      )}
     </div>
   );
 };
